cfd0187948df1d22c8ddd104cbe23bf9fd932b3d
Ketsia-a <iraketsia2001@gmail.com>
2020-12-08 10:57:11 +0200

    removing comments

--- config.py
+++ config.py
@@ -1,21 +1,18 @@
 import os
 
 class Config:
-
-    # NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?category={}&apiKey={}'
-    # ARTICLE_NEWS_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+  
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kets:ketsia321@localhost/pitchapp"
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+  
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    pass
-
+   
 
 
 class ProdConfig(Config):
