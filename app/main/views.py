fdab62095c07c0d3bc7c1dab610a6f68885a204a
Ketsia-a <iraketsia2001@gmail.com>
2020-12-08 20:48:26 +0200

    last

--- app/main/views.py
+++ app/main/views.py
@@ -22,28 +22,7 @@ def profile(uname):
    
     if user is None:
         abort(404)
-    
-    return render_template("profile/profile.html", user = user)   
-
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
-@login_required
-def update_profile(uname):
-    user = User.query.filter_by(username = uname).first()
-    if user is None:
-        abort(404)
-
-    form = UpdateProfile()
-     
-    if form.validate_on_submit():
-        user.bio = form.bio.data
-
-        db.session.add(user)
-        db.session.commit()
-
-        return redirect(url_for('.profile',uname=user.username))
-    
-
-    return render_template("profile/update.html", form =form)
+    return render_template("profile/profile.html", user = user )
 
 main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
@@ -55,6 +34,25 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)    
 
 
 
